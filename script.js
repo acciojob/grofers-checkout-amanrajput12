a77efddfa682ad523dc4fc6313dfaea92bbce2ac
Aman Rajput <89723728+amanrajput12@users.noreply.github.com>
2025-03-21 10:51:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,17 @@ const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
+let data = document.querySelectorAll(".price");
 const getSum = () => {
 //Add your code here
-  
+	let sum =0;
+	for(let i=0;i<data.length;i++){
+	sum=sum+Number(data[i].innerText)
+	}
+	let value = document.createElement("span");
+	value.innerText=sum
+  alert(value.innerText)
+	document.body.appendChild(value)
 };
 
 getSumBtn.addEventListener("click", getSum);
